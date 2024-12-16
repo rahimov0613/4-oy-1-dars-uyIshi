1da578319556c8953bd3e1fcac68813f87142acd
Abduhamid rahimov <rakhimov0613@gmail.com>
2024-12-16 23:36:02 +0500

    10-commit inport qilish

---
+++ main.js
@@ -0,0 +1,7 @@
+const { mathAdd, mathSub, mathMul, mathDiv } = require(`./index`)
+
+
+console.log(mathAdd(5, 3));
+console.log(mathSub(10, 4));
+console.log(mathMul(3, 4));
+console.log(mathDiv(8, 2)); 
