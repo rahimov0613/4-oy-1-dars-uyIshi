e1d90c7fa1754688c18ec718b205acbfd1fe76ae
Abduhamid rahimov <rakhimov0613@gmail.com>
2024-12-16 23:49:25 +0500

    13-commit os ni ishlatin

--- main.js
+++ main.js
@@ -8,3 +8,15 @@ console.log(mathSub(10, 4));
 console.log(mathMul(3, 4));
 console.log(mathDiv(8, 2));
 
+function systemMonitoring() {
+    console.log("---Tizim Monitoring---");
+    console.log("Operatsion tizim:", os.type());
+    console.log("Platforma:", os.platform());
+    console.log("Arxitektura:", os.arch());
+    console.log("Umumiy xotira", (os.totalmem() / 1024 ** 3).toFixed(2), "GB");
+    console.log("Bo'sh xotira:", (os.freemem() / 1024 ** 3).toFixed(2), "GB");
+    console.log("Ish vaqti:", (os.uptime() / 3600).toFixed(2), "soat");
+
+}
+
+systemMonitoring()
