a8246ee76717d4eb562ab2fa54ae46e77adb2f75
Abduhamid rahimov <rakhimov0613@gmail.com>
2024-12-16 23:44:53 +0500

    12-commit os ni ishlatish

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
+const os = require(`node:os`)
+
 const { mathAdd, mathSub, mathMul, mathDiv } = require(`./index`)
 
 
 console.log(mathAdd(5, 3));
 console.log(mathSub(10, 4));
 console.log(mathMul(3, 4));
-console.log(mathDiv(8, 2)); 
+console.log(mathDiv(8, 2));
+
